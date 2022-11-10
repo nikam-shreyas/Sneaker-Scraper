bbbf9bae19ced600ff1875e40987bd5b01198d78
ZeroNP <nik4m.5hreyas@gmail.com>
2022-11-10 16:01:15 -0500

    saving images and csv

--- SneakerScraper.py
+++ SneakerScraper.py
@@ -4,11 +4,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from urllib.request import urlretrieve
+import os
+import csv
 
 # Global variables
 DRIVER_PATH = 'chromedriver'
 URL = 'https://www.footlocker.com/category/shoes.html'
 IMAGE_SAVE_PATH = 'images'
+CSV_FILENAME = 'Products.csv'
 
 
 def get_images(driver, product_name):
@@ -22,7 +25,10 @@ def get_images(driver, product_name):
     images = gallery.find_elements(By.TAG_NAME, "img")
     for index, image in enumerate(images):
         src = image.get_attribute('src')
-        urlretrieve(src, IMAGE_SAVE_PATH + '/' + product_name + str(index) + '.jpg')
+        directory = IMAGE_SAVE_PATH + '/' + product_name
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+        urlretrieve(src, directory + '/' + str(index) + '.jpg')
 
 
 def visit_page(driver: webdriver.Chrome(), link: str):
@@ -39,25 +45,26 @@ def visit_page(driver: webdriver.Chrome(), link: str):
         WebDriverWait(driver, 5).until(
             EC.presence_of_element_located((By.CLASS_NAME, "ProductDetails-header"))
         )
-        # Fetch teh product details from the page
+
+        # Fetch the product details from the page
         details = driver.find_element_by_class_name("ProductDetails-header")
         product_name = details.find_element_by_class_name("ProductName-primary").text
         description = driver.find_element_by_class_name("ProductDetails-description").text
-        reviews_tab = driver.find_element_by_id("ProductDetails-tabs-reviews-tab")
-        rating = reviews_tab.find_element(By.XPATH,
-                                          '//*[@id="BVRRSummaryContainer"]/div/div/div/div/div/div/dl/dd[1]/div/a/span').text
         price = driver.find_element_by_class_name("ProductPrice").text
-        print(product_name, description, rating, price)
+
         # get images for the product
         get_images(driver, product_name)
 
     finally:
         pass
 
+    return {'ProductName': product_name, 'Description': description, 'Price': price}
 
-def main():
+
+def sneaker_scraper():
     # Load the driver and the url to crawl
     driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+    header = ['ProductName', 'Description', 'Price']
 
     # Try waiting for the page to load so that all the elements in the page have loaded
     try:
@@ -65,18 +72,30 @@ def main():
         WebDriverWait(driver, 5).until(
             EC.presence_of_element_located((By.CLASS_NAME, "ProductCard-link"))
         )
+
         # Fetch all the products on current page
         cards = driver.find_elements_by_class_name("ProductCard-link")
-        for index, card in enumerate(cards):
-            product_link = card.get_attribute('href')
-            #  Visit each product page individually
-            visit_page(driver, product_link)
-            if index == 5:
-                break
+
+        # Store the links for the products on the current page in a list
+        product_links = []
+        for product in cards:
+            product_links.append(product.get_attribute('href'))
+
+        # Iterate over the links
+        with open(CSV_FILENAME, 'w+') as csv_file:
+
+            writer = csv.DictWriter(csv_file, fieldnames=header)
+            writer.writeheader()
+
+            for index, product_link in enumerate(product_links):
+
+                #  Visit each product page individually
+                product_data = visit_page(driver, product_link)
+                writer.writerow(product_data)
+
+                # Sampling for 5 products
+                if index == 5:
+                    break
 
     finally:
         driver.quit()
-
-
-if __name__ == '__main__':
-    main()
